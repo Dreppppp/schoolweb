28582a05e04e7ff682f794a1ee25de6805ce3938
VanyaIvasuk <vanko20111.1@gmail.com>
2025-12-03 22:32:33 +0200

    Updates

--- _next/static/OpxMjo7Jax78ijffosoT1/_buildManifest.js
+++ _next/static/OpxMjo7Jax78ijffosoT1/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/schoolwev//_next/:path+",
+        "destination": "/schoolweb/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
