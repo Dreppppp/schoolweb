2bcd11ae7e7ad87eb280c9116bb0a30b07f36e83
VanyaIvasuk <vanko20111.1@gmail.com>
2025-12-03 23:28:10 +0200

    fixed mistake

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true, // або без цього
+  },
+  // НІЯКОГО basePath !!!
 };
 
-export default nextConfig;
+module.exports = nextConfig;
