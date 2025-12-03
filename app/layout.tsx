9be145ba7b96579a57a3b965d72aa35943fe611c
VanyaIvasuk <vanko20111.1@gmail.com>
2025-12-03 23:44:26 +0200

    Fix tailwind paths

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,21 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: "class",
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+import type { Metadata } from "next";
+import "./globals.css";
+import AOSProvider from "@/providers/AOSprovider";
+
+export const metadata: Metadata = {
+  title: "Школа",
 };
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="uk">
+      <body className="bg-white text-black dark:bg-black dark:text-white antialiased">
+        <AOSProvider>{children}</AOSProvider>
+      </body>
+    </html>
+  );
+}
